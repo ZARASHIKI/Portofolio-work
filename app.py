760d062da89eb9cb66efc0883bb9676808c250ea
ZARASHIKI <agusyusup69@gmail.com>
2024-03-20 12:34:31 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,request,jsonify
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/send_email', methods=['POST'])
+def send_email():
+    name = request.form['fname']
+    name2 = request.form['lname']
+    email = request.form['email']
+    message = request.form['message']
+        
+    sender_email = "agusyusup69@gmail.com"
+    password = "qsit jepf axjg skie "
+    
+    subject = "New Message from Contact Form"
+
+    msg = MIMEMultipart()
+    msg['From'] = "Client"
+    msg['To'] = sender_email
+    msg['Subject'] = subject
+    
+    body = f"Name: {name} {name2}\nEmail: {email}\nMessage:\n{message}"
+    msg.attach(MIMEText(body, 'plain'))
+    
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(sender_email, password)
+        server.send_message(msg)
+
+    
+    return jsonify({'result':'success'})
+
+
+if __name__ == '__main__':  
+    app.run('0.0.0.0',port=5000,debug=True)
---
+++ static/main.js
@@ -0,0 +1,60 @@
+function toggleMenu() {
+    var closedIcon = document.getElementById('nav-icon1');
+    closedIcon.classList.toggle('open');
+    var mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+    mobileMenu.classList.toggle('open');
+}
+function send_email() {
+    let submitButton = $('#submit');
+    let loadingButton = $('#loading');
+
+    submitButton.addClass('hidden');
+    loadingButton.removeClass('hidden');
+    let fname = $('#first_name').val();
+    let lname = $('#last_name').val();
+    let email = $('#email').val();
+    let message = $('#message').val();
+    $.ajax({
+        type: "POST",
+        url: "/send_email",
+        data: {
+            fname: fname,
+            lname: lname,
+            email: email,
+            message: message,
+        },
+        success: function (response) {
+            submitButton.removeClass('hidden');
+            loadingButton.addClass('hidden');
+    
+
+
+            alert("Thank you for your message. We will get back to you soon!")
+            window.location.replace("/")
+        },
+    });
+}
+$(document).ready(function() {
+  $('.smth').click(function(event) {
+      event.preventDefault();
+
+      var target = $($(this).attr('href'));
+
+      $('html, body').animate({
+          scrollTop: target.offset().top
+      }, 1000);
+  });
+});
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'all-bg': '#0F0F0F',
+          'white':'#D2D2D2',
+          'primary-red': 'var(--Primary-Red, #921616)',
+        },
+       
+      }
+    }
+  }
